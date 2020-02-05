6cb58eed0d2869ae8f8ff9847c571070422b27c6
Daniel Ellis <danielshawellis@gmail.com>
2020-02-05 09:38:50 -0600

    add headers and basic plugin meta information

---
+++ ammonite-custom-drs.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Plugin Name:       Ammonite Custom DRS
+ * Description:       Adds a shortcode to handle custom Doctor's Reference Sheets
+ * Version:           1.0.0
+ * Author:            Daniel Ellis
+ * Author URI:        https://danielellisdevelopment.com/
+ */
